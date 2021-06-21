f4651e47bbc25a3dced5d817e9e8339efd1d1d32
Moroz <moroz9999@mail.ru>
2021-06-21 19:12:53 +0300

    zaglushka02 added

--- les01_task02.py
+++
@@ -1,8 +0,0 @@
-# Task 2
-a = b"class"
-b = b"function"
-c = b"method"
-
-arr_abc = [a, b, c]
-for i in range(len(arr_abc)):
-    print(type(arr_abc[i]), arr_abc[i], len(arr_abc[i]))
--- les01_task04.py
+++
@@ -1,6 +0,0 @@
-# Task 4
-arr2 = ["разработка", "администрирование", "protocol", "standard"]
-for i in range(len(arr2)):
-    x_enc = arr2[i].encode('utf-8')
-    x_dec = x_enc.decode('utf-8')
-    print(x_enc, x_dec)
